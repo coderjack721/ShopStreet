8d2df20a343dd05d6a820ea1d20894b82212f71e
coderjack721 <1120128678@qq.com>
2021-08-14 11:16:54 +0800

    final

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <Swiper>
+    <swiper-item v-for="(item,iid) in banners" :key="iid">
+      <a :href="item.link">
+        <img :src="item.image" alt="" @load="imageLoad">
+      </a>
+    </swiper-item>
+  </Swiper>
+</template>
+
+<script>
+  import {Swiper,SwiperItem} from 'components/common/swiper/index.js'
+  export default {
+    name: "HomeSwiper",
+    props:{
+      banners:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    },
+    data(){
+      return {
+        isLoad:false
+      }
+    },
+    components:{
+      Swiper,
+      SwiperItem
+    },
+    methods:{
+      imageLoad(){
+        if(!this.isLoad){
+          this.$emit('swiperImageLoad')
+          this.isLoad = true;
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
